dd6c4a21206c508a1a56c18a05a88718690ca851
Diego Kiedanski <dkiedanski@gmail.com>
2022-03-27 14:00:15 -0400

    Bump version: 0.2.1 → 0.2.2

--- pygradus/config.py
+++ pygradus/config.py
@@ -1,9 +1,12 @@
 from dynaconf import Dynaconf
+from importlib import resources
 
-settings = Dynaconf(
-    envvar_prefix="DYNACONF",
-    settings_files=["settings.toml", ".secrets.toml"],
-)
+
+with resources.path("pygradus", "settings.toml") as path:
+    settings = Dynaconf(
+        envvar_prefix="DYNACONF",
+        settings_files=[path],
+    )
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
